bed745a3b2f147d8bb51cac072be7e6901710c33
Jan Leirvaag <lja022025@gmail.com>
2025-04-23 11:41:13 +0200

    added class

--- Program.cs
+++ Program.cs
@@ -7,11 +7,27 @@ namespace SQLTest2;
 
 class Program
 {
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+
+    }
+
     static void Main(string[] args)
     {
+        //Instantiate Person object
+        Person person = new Person();
+
         ////person is the table
         string query = "SELECT * FROM person";
-        string insertQuery = "INSERT INTO person (first_name, last_name, phone, email, street, city, country, zip_code) VALUES (@f_name, @l_name, @phone, @email, @street, @country, @city, @zip)";
+        string insertQuery = "INSERT INTO person (first_name, last_name, phone, email, street, city, zip_code, country) VALUES (@f_name, @l_name, @phone, @email, @street, @city, @zip, @country)";
 
         //TestDB is the one we created via SQL Server Management Studio (SSMS)         
         string connectionString = "Server=localhost\\SQLEXPRESS;Database=TestDB;Trusted_Connection=True;TrustServerCertificate=true";
@@ -33,12 +49,6 @@ class Program
                 //Open the DB connection            
                 conn.Open();
 
-                //Instantiate SELECT and INSERT SQL Objects
-                // using var select = new SqlCommand(query, conn);
-                // using var insert = new SqlCommand(query, conn);
-
-                // using var reader = select.ExecuteReader();
-
                 //Choose based on user choice
                 switch (input)
                 {
@@ -49,7 +59,7 @@ class Program
                             using var reader = select.ExecuteReader();
 
                             while (reader.Read())
-                                Console.WriteLine($"{reader["person_id"]} \t{reader["first_name"]} \t{reader["last_name"]} \t{reader["email"]}");
+                                Console.WriteLine($"{reader["person_id"]} \t{reader["first_name"]} \t{reader["last_name"]} \t{reader["phone"]} \t{reader["email"]} \t{reader["street"]} \t{reader["city"]} \t{reader["zip_code"]} \t{reader["country"]}");
 
                             //add some space
                             Console.WriteLine();
@@ -57,18 +67,51 @@ class Program
                         }
                     //Insert 
                     case "2":
+                        string userInsert;
+                        string temp;
                         {
                             using var insert = new SqlCommand(insertQuery, conn);
 
-                            insert.Parameters.AddWithValue("@f_name", "John");
-                            insert.Parameters.AddWithValue("@l_name", "Doe");
-                            insert.Parameters.AddWithValue("@phone", "");
-                            insert.Parameters.AddWithValue("@email", "john_doe@mail.com");
-                            insert.Parameters.AddWithValue("@street", "Teststree 1");
-                            insert.Parameters.AddWithValue("@country", "Sosaria");
-                            insert.Parameters.AddWithValue("@city", "Britannia");
-                            insert.Parameters.AddWithValue("@zip", "0000");
+                            Console.WriteLine("First name: ");
+                            userInsert = Console.ReadLine();
+                            person.FirstName = userInsert;
+                            Console.WriteLine("Last name: ");
+                            userInsert = Console.ReadLine();
+                            person.LastName = userInsert;
+                            Console.WriteLine("Phone: ");
+                            userInsert = Console.ReadLine();
+                            person.Phone = userInsert;
+                            Console.WriteLine("Email: ");
+                            userInsert = Console.ReadLine();
+                            person.Email = userInsert;
+                            Console.WriteLine("Street name: ");
+                            userInsert = Console.ReadLine();
+                            person.Street = userInsert;
+                            Console.WriteLine("City: ");
+                            userInsert = Console.ReadLine();
+                            person.City = userInsert;
+                            Console.WriteLine("Zip code (max 5 digits): ");
 
+                            //Max 5 char in TestDB
+                            temp = Console.ReadLine();
+                            userInsert = temp.Remove(5);
+
+                            person.ZipCode = userInsert;
+                            Console.WriteLine("Country: ");
+                            userInsert = Console.ReadLine();
+                            person.Country = userInsert;
+
+                            //Add to SQL insert
+                            insert.Parameters.AddWithValue("@f_name", person.FirstName);
+                            insert.Parameters.AddWithValue("@l_name", person.LastName);
+                            insert.Parameters.AddWithValue("@phone", person.Phone);
+                            insert.Parameters.AddWithValue("@email", person.Email);
+                            insert.Parameters.AddWithValue("@street", person.Street);
+                            insert.Parameters.AddWithValue("@city", person.City);
+                            insert.Parameters.AddWithValue("@zip", person.ZipCode);
+                            insert.Parameters.AddWithValue("@country", person.Country);
+
+                            //Run query
                             insert.ExecuteNonQuery();
                             break;
                         }
